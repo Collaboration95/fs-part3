ead0a2a63f92bb73d27c6aecb6a3b00a5d17f7cb
Guruprasath <guruprasath3200@gmail.com>
2023-09-20 20:49:50 +0800

    Finished Part3/3.13,3.14,3.14,3.15

--- phbook-backend/index.js
+++ phbook-backend/index.js
@@ -2,16 +2,32 @@ const mongoose = require('mongoose')
 const express = require('express');
 require('dotenv').config()
 const Entry = require('./models/note')
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const app = express()
 const cors = require('cors')
 app.use(cors())
-app.use(express.json())
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+}
 
+app.use(express.static('build'))
+app.use(express.json())
+app.use(requestLogger)
 app.use(express.static('dist'))
 
-morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+
+
+// morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+// app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   { 
@@ -62,10 +78,15 @@ app.get('/info',(request,response)=>{
     
 }
 )
-app.delete('/api/persons/delete/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(note => note.id !== id)
-    response.status(204).end()
+app.delete('/api/persons/delete/:id', (request, response,next) => {
+    const id = String(request.params.id)
+
+    Entry.findByIdAndRemove(id)
+    .then(result => {
+      response.status(204).end("Success")
+    }
+    )
+    .catch(error => next(error))
 }
 )
 
@@ -94,19 +115,48 @@ const generateId = () => {
     })
       })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id =Number(request.params.id);
+app.get('/api/persons/:id', (request, response,next) => {
+  const id = String(request.params.id)
   Entry.findById(id).then(entry => {
     if (entry) {
-      // response.json(entry)
-      response.json("entry")
+        response.json(entry)
     }
     else {
-    console.log("entry not found")
+        response.status(404).end( "Not found")
     }
   })
-  })
-  
+  .catch(error => next(error))
+})
+// app.put('/api/persons/:id', (request, response, next) => {
+//   const body = request.body
+
+//   const entry = {
+//     name: body.name,
+//     number: body.number,
+//     id:generateId(),
+//   }
+//   console.log(request.params.id)
+//  Entry.findByIdAndUpdate(request.params.id, entry, { new: true })
+//     .then(updatedNote => {
+//       response.json(updatedNote)
+//     })
+//     .catch(error => next(error))
+// })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+app.use(unknownEndpoint)
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
+
 const PORT =process.env.PORT || 3001
     app.listen(PORT)
     console.log(`Server running on port ${PORT}`)
